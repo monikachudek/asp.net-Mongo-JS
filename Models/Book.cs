e71ef13c1293c8a6021e944f09ffd0c0377df5eb
Monika <monika.chudek1@gmail.com>
2020-02-26 10:25:57 +0100

    Configure JSON

--- Models/Book.cs
+++ Models/Book.cs
@@ -1,5 +1,6 @@
 ﻿using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
+using Newtonsoft.Json;
 
 namespace MongoBooksApi.Models
 {
@@ -10,6 +11,7 @@ namespace MongoBooksApi.Models
         public string Id { get; set; }
 
         [BsonElement("Name")]
+        [JsonProperty("Name")]
         public string BookName { get; set; }
 
         public decimal Price { get; set; }
--- Startup.cs
+++ Startup.cs
@@ -36,7 +36,8 @@ namespace MongoBooksApi
 
             services.AddSingleton<BookService>();
 
-            services.AddControllers();
+            services.AddControllers()
+                .AddNewtonsoftJson(options => options.UseMemberCasing());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
